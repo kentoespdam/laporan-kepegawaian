a3f95184f9ca094d0ac001692197fe3c7fd9ee88
Kentoes PDAM <kentoes.pdam@gmail.com>
2025-10-10 10:08:01 +0700

    remove static and views

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-# from fastapi.staticfiles import StaticFiles
+
 from app.routes import main
 
 app = FastAPI(
     title="Laporan Kepegawaian"
 )
-
-# laporan_kepegawaian.mount("/public", StaticFiles(directory="static/public"), name="public")
 
 app.add_middleware(
     CORSMiddleware,
